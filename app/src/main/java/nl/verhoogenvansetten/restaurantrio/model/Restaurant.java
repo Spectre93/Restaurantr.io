301efc53d5eb5a6402a0e3cce062c9fd6441e5e2
Jori <vandamdevelopment@gmail.com>
2016-09-21 13:55:15 +0200

    Extended the Restaurant class to have a constructor for new objects and for the instantiation of DB objects + Added insertRestaurant function in the DatabaseHelper class

--- app/src/main/java/nl/verhoogenvansetten/restaurantrio/model/Restaurant.java
+++ app/src/main/java/nl/verhoogenvansetten/restaurantrio/model/Restaurant.java
@@ -1,8 +1,102 @@
 package nl.verhoogenvansetten.restaurantrio.model;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v4.content.ContextCompat;
+
+import nl.verhoogenvansetten.restaurantrio.R;
+import nl.verhoogenvansetten.restaurantrio.util.DatabaseHelper;
+import nl.verhoogenvansetten.restaurantrio.util.DbBitmapUtility;
+
 /**
  * Created by Jori on 20-9-2016.
  */
 
-public class Restaurant {
+public class Restaurant{
+    private Context context;
+    private long id;
+    private String name;
+    private String location;
+    private String description;
+    private Bitmap image;
+
+    //Constructor which is used to create new restaurant objects and adds them to the db.
+    public Restaurant(Context context, String name, String location, String description, Bitmap image) {
+        this.context = context;
+        this.name = name;
+        this.location = location;
+        this.description = description;
+        this.image = image;
+        DatabaseHelper dbHelper = new DatabaseHelper(this.context);
+        long result = dbHelper.insertRestaurant(this.getName(), this.getLocation(),
+                this.getDescription(), this.getByteArrayFromImage());
+        //If the insert is successful
+        if (result != -1){
+            this.id = result;
+        }else{
+            //// TODO: 21-9-2016  
+        }
+
+    }
+
+    //Constructor which is used to load existing restaurant objects from the DB.
+    public Restaurant(Context context, int id, String name, String location, String description, Bitmap image) {
+        this.context = context;
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.description = description;
+        this.image = image;
+    }
+
+    public Context getContext(){
+        return this.context;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public byte[] getByteArrayFromImage(){
+        return DbBitmapUtility.getBytes(this.getImage());
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public void setImageFromByteArray(byte[] image) {
+        this.image = DbBitmapUtility.getImage(image);
+    }
+
+
+
 }
--- app/src/main/java/nl/verhoogenvansetten/restaurantrio/util/DatabaseHelper.java
+++ app/src/main/java/nl/verhoogenvansetten/restaurantrio/util/DatabaseHelper.java
@@ -1,8 +1,62 @@
 package nl.verhoogenvansetten.restaurantrio.util;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Jori on 20-9-2016.
  */
 
-public class DatabaseHelper {
+public class DatabaseHelper extends SQLiteOpenHelper {
+    //Database version
+    private static final int DATABASE_VERSION = 1;
+
+    //Database name
+    private static final String DATABASE_NAME = "restaurantrio.db";
+
+    //Database table names
+    private static final String TABLE_RESTAURANT = "table_restaurant";
+    //Database column names for the TABLE_RESTAURANT
+    private static final String COLUMN_RESTAURANT_ID = "id";
+    private static final String COLUMN_RESTAURANT_NAME = "name";
+    private static final String COLUMN_RESTAURANT_LOCATION = "location";
+    private static final String COLUMN_RESTAURANT_DESCRIPTION = "description";
+    private static final String COLUMN_RESTAURANT_IMAGE = "image";
+
+    //Table creation statement
+    private static final String CREATE_TABLE_RESTAURANT = "CREATE TABLE " + TABLE_RESTAURANT + "(" +
+            COLUMN_RESTAURANT_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            COLUMN_RESTAURANT_NAME + " TEXT," +
+            COLUMN_RESTAURANT_LOCATION + " TEXT," +
+            COLUMN_RESTAURANT_DESCRIPTION + " TEXT," +
+            COLUMN_RESTAURANT_IMAGE + " BLOB);";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    //Adds the restaurant to the DB, returns the id or -1 when it failed.
+    public long insertRestaurant(String name, String location, String description, byte[] image){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_RESTAURANT_NAME, name);
+        contentValues.put(COLUMN_RESTAURANT_LOCATION, location);
+        contentValues.put(COLUMN_RESTAURANT_DESCRIPTION, description);
+        contentValues.put(COLUMN_RESTAURANT_IMAGE, image);
+        long result = db.insert(TABLE_RESTAURANT, null, contentValues);
+        return result;
+    }
+
 }
