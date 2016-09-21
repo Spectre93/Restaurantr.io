98bfa4624766d2ea0c6247fd26215124ae176d52
Daniel Jansen <djaniboe@gmail.com>
2016-09-21 13:39:11 +0200

    Add Restaurant default constructor and private fields

--- app/src/main/java/nl/verhoogenvansetten/restaurantrio/model/Restaurant.java
+++ app/src/main/java/nl/verhoogenvansetten/restaurantrio/model/Restaurant.java
@@ -1,8 +1,24 @@
 package nl.verhoogenvansetten.restaurantrio.model;
 
+import java.sql.Blob;
+
 /**
  * Created by Jori on 20-9-2016.
  */
 
 public class Restaurant {
+
+    private int id;
+    private String name;
+    private String location;
+    private String description;
+    private Blob image;
+
+    public Restaurant(int id, String name, String location, String description, Blob image){
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.description = description;
+        this.image = image;
+    }
 }
