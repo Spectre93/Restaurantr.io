5f684fd1b6e7c83cedfd4cb62abe4f1c7682e97d
Daniel Jansen <djaniboe@gmail.com>
2016-09-21 13:38:34 +0200

    Add DatabaseHelper dummy functions

--- app/src/main/java/nl/verhoogenvansetten/restaurantrio/util/DatabaseHelper.java
+++ app/src/main/java/nl/verhoogenvansetten/restaurantrio/util/DatabaseHelper.java
@@ -1,8 +1,43 @@
 package nl.verhoogenvansetten.restaurantrio.util;
 
+import java.sql.Blob;
+import java.util.ArrayList;
+
+import nl.verhoogenvansetten.restaurantrio.model.Restaurant;
+
 /**
  * Created by Jori on 20-9-2016.
  */
 
-public class DatabaseHelper {
+public final class DatabaseHelper {
+
+    // Private constructor so the class can't be instantiated
+    private DatabaseHelper(){
+    }
+
+    public static Restaurant getRestaurantById(int id){
+        return null;
+    }
+
+    // Returns the entire list of restaurants
+    public static ArrayList<Restaurant> getRestaurantList(){
+        return null;
+    }
+
+    // Returns only the restaurants whose names match the query
+    public static ArrayList<Restaurant> getRestaurantList(String query){
+        return null;
+    }
+
+    public static boolean addRestaurant(String name, String location, String description, Blob image){
+        return false;
+    }
+
+    public static boolean editRestaurant(int id, String name, String location, String description, Blob image){
+        return false;
+    }
+
+    public static boolean deleteRestaurant(int id){
+        return false;
+    }
 }
